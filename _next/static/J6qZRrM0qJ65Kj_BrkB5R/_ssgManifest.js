a9865d73d997d4ac13f6b7b61c7e6958a242a8f7
randomgoose <randomgoose@users.noreply.github.com>
2023-12-18 11:00:19 +0000

    deploy: wandfi/wand-ui@a58e9d7578ed217f8ea69f454b953be89f55f852

---
+++ _next/static/J6qZRrM0qJ65Kj_BrkB5R/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fearn\u002F[slug]","\u002Fvaults\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/chunks/app/vaults/layout-7a0c676b6b827241.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7744],{46472:function(e,r,t){Promise.resolve().then(t.bind(t,73397))},73397:function(e,r,t){"use strict";t.r(r),t.d(r,{default:function(){return o}});var n=t(9268);function o(e){let{children:r}=e;return(0,n.jsx)("div",{className:"pt-28 bg-slate-50",children:(0,n.jsx)("div",{className:"w-full my-12 max-w-screen-xl px-4 mx-auto",children:r})})}},83177:function(e,r,t){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=t(86006),o=Symbol.for("react.element"),s=Symbol.for("react.fragment"),u=Object.prototype.hasOwnProperty,f=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,c={key:!0,ref:!0,__self:!0,__source:!0};function i(e,r,t){var n,s={},i=null,l=null;for(n in void 0!==t&&(i=""+t),void 0!==r.key&&(i=""+r.key),void 0!==r.ref&&(l=r.ref),r)u.call(r,n)&&!c.hasOwnProperty(n)&&(s[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===s[n]&&(s[n]=r[n]);return{$$typeof:o,type:e,key:i,ref:l,props:s,_owner:f.current}}r.Fragment=s,r.jsx=i,r.jsxs=i},9268:function(e,r,t){"use strict";e.exports=t(83177)}},function(e){e.O(0,[9253,2961,1744],function(){return e(e.s=46472)}),_N_E=e.O()}]);
